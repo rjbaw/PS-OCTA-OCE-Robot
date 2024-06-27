7a05ee28166e4b733ab3aac1335afd4fd9ef1d24
rjbaw <rjbaw@uw.edu>
2024-06-26 17:29:55 -0700

    added ur ros2 driver and rti dds

---
+++ ros2_driver/env.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+source /opt/ros/iron/setup.bash
+export RMW_IMPLEMENTATION=rmw_connextdds
+source /opt/rti.com/rti_connext_dds-6.0.1/resource/scripts/rtisetenv_x64Linux4gcc7.3.0.bash
+#source /opt/rti_connext_dds-6.0.1/resource/scripts/rtisetenv_x64Linux4gcc7.3.0.bash
+
