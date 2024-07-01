7bb53a26565c09985795df4da85ef889130004e3
rjbaw <rjbaw@uw.edu>
2024-07-01 14:37:47 -0700

    change ros version

--- ros2_driver/setup.sh
+++ ros2_driver/setup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 sudo apt-get update
-sudo apt-get install -y rti-connext-dds-6.0.1 ros-iron-rmw-connextdds iproute2 iputils-ping traceroute vim
+sudo apt-get install -y rti-connext-dds-6.0.1 ros-${ROS_DISTRO}-rmw-connextdds iproute2 iputils-ping traceroute vim ros-${ROS_DISTRO}-ur
