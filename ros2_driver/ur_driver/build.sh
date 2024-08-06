2ae3d5cbe59ee59b6ff8fab82b6677cd2662ff48
rjbaw <rjbaw@uw.edu>
2024-08-05 17:56:41 -0700

    corrected build path

--- ros2_driver/ur_driver/build.sh
+++ ros2_driver/ur_driver/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-vcs import src --skip-existing --input src/Universal_Robots_ROS2_Driver/Universal_Robots_ROS2_Driver-not-released.${ROS_DISTRO}.repos
+vcs import src --skip-existing --input src/ur_ros2/Universal_Robots_ROS2_Driver-not-released.${ROS_DISTRO}.repos
 rosdep update
 rosdep install --ignore-src --from-paths src -y
 colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release
--- ros2_driver/ur_driver/launch.sh
+++ ros2_driver/ur_driver/launch.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-ros2 launch ur_robot_driver ur_control.launch.py ur_type:=ur3e robot_ip:=192.168.0.10 launch_rviz:=true robot_controller:=scaled_joint_trajectory_controller kinematics_params_files:="./ur_calibration.yaml" reverse_ip:=192.168.0.5 headless:=true
+ros2 launch ur_robot_driver ur_control.launch.py ur_type:=ur3e robot_ip:=192.168.0.10 launch_rviz:=false robot_controller:=scaled_joint_trajectory_controller kinematics_params_files:="./ur_calibration.yaml" reverse_ip:=192.168.0.5 #headless:=true
 
 #ros2 launch ur_robot_driver ur_control.launch.py ur_type:=ur3e robot_ip:=192.168.56.101 launch_rviz:=true robot_controller:=scaled_joint_trajectory_controller kinematics_params_files:="./ur_calibration.yaml" activate_joint_controller:=true
