43c7e331507b2a2ca253966158f54355fb71ad05
rjbaw <rjbaw@uw.edu>
2024-09-27 14:23:57 -0700

    clean up VI and misc

--- matlab_scripts/cal_rot.m
+++
@@ -1,9 +0,0 @@
-msg_format = 'def program():\n global pose_wrt_tool = p[%01.4f,%01.4f,%01.4f,%01.4f,%01.4f,%01.4f]\n global pose_wrt_base = pose_trans(get_forward_kin(), pose_wrt_tool)\n movel(pose_wrt_base, a=%01.2f, v=%01.2f)\nend\n';
-%msg_format = 'def program():\n global pose_wrt_tool = p[%01.4f,%01.4f,%01.4f,%01.4f,%01.4f,%01.4f]\n global angle_axis = rotvec2rpy([pose_wrt_tool[3], pose_wrt_tool[4], pose_wrt_tool[5]])\n pose_wrt_tool[3] = angle_axis[3]\n pose_wrt_tool[4] = angle_axis[4]\n pose_wrt_tool[5] = angle_axis[5]\n global pose_wrt_base = pose_trans(get_forward_kin(), pose_wrt_tool)\n movel(pose_wrt_base, a=%01.2f, v=%01.2f)\nend\n';
-
-dx=0;dy=0;
-%dRx=0;dRy=-dR;dRz=0;
-dRx=-dR*0.75;dRy=0;dRz=0;
-
-
-msg = sprintf(msg_format, [dx,dy,dz,dRx,dRy,dRz,acc,vel])
--- matlab_scripts/com_robot_savepoint.m
+++
@@ -1,3 +0,0 @@
-msg_format = 'def MyProg():\n global home_pose = get_forward_kin()\n global a = home_pose[0]\n global a1 = home_pose[1]\n global a2 = home_pose[2]\n global a3 = home_pose[3]\n global a4 = home_pose[4]\n global a5 = home_pose[5]\n write_output_float_register(1, a)\n write_output_float_register(2, a1)\n write_output_float_register(3, a2)\n write_output_float_register(4, a3)\n write_output_float_register(5, a4)\n write_output_float_register(6, a5)\nend\n'
-
-msg = sprintf(msg_format)
