683a156efe5ef63cabea71e446ec1c87dfbdd961
rjbaw <raveeroj.b@rjbl.dev>
2025-01-01 08:00:44 -0500

    update detection algorithm without fft

--- ros2_driver/test/test_detect_cpp.sh
+++ ros2_driver/test/test_detect_cpp.sh
@@ -2,6 +2,8 @@
 #DATASET_NAME="no_background"
 DATASET_NAME="current"
 RESULT_DIR="result"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd ${SCRIPT_DIR}
 source ../install/setup.bash
 rm -fr data/${RESULT_DIR}
 mkdir -p data/${RESULT_DIR}
@@ -10,3 +12,4 @@ for f in data/${DATASET_NAME}/*; do
     filename=$(basename "${f}")
     ros2 run octa_ros test_detect "${f}" "data/${RESULT_DIR}/${filename}"
 done
+cd -
