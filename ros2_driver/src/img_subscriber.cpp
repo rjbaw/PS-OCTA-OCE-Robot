dfc59ad80af71ab96fdb4003616b4cddbb81fa0c
rjbaw <raveeroj.b@rjbl.dev>
2025-01-07 18:36:48 -0500

    get_img blocking

--- ros2_driver/src/img_subscriber.cpp
+++ ros2_driver/src/img_subscriber.cpp
@@ -7,62 +7,76 @@ img_subscriber::img_subscriber()
     subscription_ = this->create_subscription<octa_ros::msg::Img>(
         "oct_image", best_effort,
         std::bind(&img_subscriber::imageCallback, this, std::placeholders::_1));
+
     timer_ = this->create_wall_timer(
         std::chrono::milliseconds(500),
         std::bind(&img_subscriber::timerCallback, this));
 }
 
 cv::Mat img_subscriber::get_img() {
+
     std::unique_lock<std::mutex> lock(img_mutex_);
-    img_status_.wait(lock, [this] { return new_img_; });
-    cv::Mat img_copy = img_.clone();
-    new_img_ = false;
-    return img_copy;
+
+    img_cv_.wait(lock, [this]() { return (img_seq_ > last_read_seq_); });
+
+    last_read_seq_ = img_seq_;
+    cv::Mat image_copy = img_.clone();
+    return image_copy;
 }
 
 void img_subscriber::imageCallback(const octa_ros::msg::Img::SharedPtr msg) {
+
     auto now = this->now();
     double elapsed = (now - last_store_time_).seconds();
-    if (elapsed < 0.4) {
+    if (elapsed < gating_interval_) {
+
         RCLCPP_INFO(this->get_logger(),
-                    "Skipping frame (%.2f seconds since last stored image)",
-                    elapsed);
+                    "Skipping frame (%.2f sec since last store)", elapsed);
         return;
     }
 
-    RCLCPP_INFO(
-        this->get_logger(),
-        "Subscribing to image (length: %zu). Elapsed: %.2f sec, storing now.",
-        msg->img.size(), elapsed);
+    RCLCPP_INFO(this->get_logger(),
+                "Storing new frame after %.2f sec (size=%zu)", elapsed,
+                msg->img.size());
+
     cv::Mat new_img(height_, width_, CV_8UC1);
     std::copy(msg->img.begin(), msg->img.end(), new_img.data);
 
     {
         std::lock_guard<std::mutex> lock(img_mutex_);
+
         img_ = new_img;
+
+        ++img_seq_;
+
         last_store_time_ = now;
-        new_img_ = true;
-        img_status_.notify_one();
+
+        img_cv_.notify_all();
     }
 }
 
 void img_subscriber::timerCallback() {
+
     cv::Mat image_to_process;
     {
         std::lock_guard<std::mutex> lock(img_mutex_);
+
         if (img_.empty()) {
-            RCLCPP_DEBUG(
-                this->get_logger(),
-                "No new image to process at this timer tick (img_ is empty)");
+            RCLCPP_INFO(this->get_logger(),
+                        "timerCallback: No image to process");
             return;
         }
+
         image_to_process = img_.clone();
     }
+
     cv::Mat current_hash;
     cv::img_hash::AverageHash::create()->compute(image_to_process,
                                                  current_hash);
     double diff = cv::norm(img_hash_, current_hash);
+
     RCLCPP_INFO(this->get_logger(),
                 "Timer triggered - processing image. Hash diff = %.2f", diff);
+
     img_hash_ = current_hash;
 }
