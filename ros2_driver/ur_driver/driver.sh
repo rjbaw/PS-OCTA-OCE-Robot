1f04e91cfa655b5bae66d2623ebd54e05319d491
rjbaw <rjbaw@uw.edu>
2025-02-07 16:11:26 -0800

    changed message type for run_state and added new bags

--- ros2_driver/ur_driver/driver.sh
+++ ros2_driver/ur_driver/driver.sh
@@ -28,9 +28,9 @@ RVIZ=true
 
 source install/setup.bash
 if  [[ $sim == "true" ]]; then
-	ros2 launch ur_robot_driver ur_control.launch.py ur_type:=ur3e robot_ip:=192.168.56.101 launch_rviz:=$RVIZ robot_controller:=scaled_joint_trajectory_controller kinematics_params_files:="./ur_calibration.yaml" headless_mode:=true
+	ros2 launch ur_robot_driver ur_control.launch.py ur_type:=ur3e robot_ip:=192.168.56.101 launch_rviz:=$RVIZ initial_joint_controller:=scaled_joint_trajectory_controller kinematics_params_files:="./ur_calibration.yaml" headless_mode:=true
 else
-	ros2 launch ur_robot_driver ur_control.launch.py ur_type:=ur3e robot_ip:=192.168.0.10 launch_rviz:=$RVIZ robot_controller:=scaled_joint_trajectory_controller reverse_ip:=192.168.0.5 kinematics_params_files:="./ur_calibration.yaml" headless_mode:=true
+	ros2 launch ur_robot_driver ur_control.launch.py ur_type:=ur3e robot_ip:=192.168.0.10 launch_rviz:=$RVIZ initial_joint_controller:=scaled_joint_trajectory_controller reverse_ip:=192.168.0.5 kinematics_params_files:="./ur_calibration.yaml" headless_mode:=true
 fi
 
 
